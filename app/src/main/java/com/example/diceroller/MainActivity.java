b439f7273fa2c61ebf6ced82d4924bef744f8ec4
Erik <1821630@brunel.ac.uk>
2019-10-24 20:39:58 +0100

    New button added

--- app/src/main/java/com/example/diceroller/MainActivity.java
+++ app/src/main/java/com/example/diceroller/MainActivity.java
@@ -16,6 +16,10 @@ import android.view.MenuItem;
 import android.view.View.OnClickListener;
 import android.widget.TextView;
 
+import java.lang.reflect.Array;
+import java.nio.charset.Charset;
+import java.security.SecureRandom;
+import java.util.Arrays;
 import java.util.Random;
 
 
@@ -67,6 +71,8 @@ public class MainActivity extends AppCompatActivity {
     TextView input; // Widget for user input
     TextView button; // Widget for dice roll button
     TextView count; // Widget to hold the counter
+    TextView breaker; // Widget for d-icebreaker
+    TextView random;
     int score = 0; // Score counter
     int result;
     String numberInsert;
@@ -83,20 +89,21 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void on_button_click(View view) {
+    public void roll_the_dice(View view) {
+
+        breaker = this.findViewById(R.id.dbreaker);
+        random = this.findViewById(R.id.randomQ);
+
 
         input();
 
         Random r = new Random(); // Creating random Object
         int number = r.nextInt(7); // Assigning value to number, the random generator
 
+
         count = this.findViewById(R.id.textView3);
-
-
         button = this.findViewById(R.id.button); // Attaching to the button
         button.setText(Integer.toString(number));
-
-
         txt = this.findViewById(R.id.congrats); // Attaching to the Congrats msg box
 
 
@@ -105,15 +112,9 @@ public class MainActivity extends AppCompatActivity {
             score++; //Increment score by 1
             count.setText(Integer.toString(score));
             button.setText(Integer.toString(number)); // displays numbers on button in String format
+            txt.setText("Congratulations");
 
-
-                    txt.setText("Congratulations");
-
-
-
-        }
-
-        else if (number != result){
+        } else if (number != result) {
 
             count.setText(Integer.toString(score));
             button.setText(Integer.toString(number)); // displays numbers on button in String format
@@ -125,6 +126,29 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+
+    public void QuestionsButton(View V) {
+
+        breaker = this.findViewById(R.id.dbreaker);
+        random = this.findViewById(R.id.randomQ);
+
+        String [] Questions = new String[6];
+
+        Questions[0] = "If you could go anywhere in the world, where would you go?";
+        Questions[1] = "If you were stranded on a desert island, what three things" +
+                "would you want to take with you?";
+        Questions[2] = "If you could eat only one food for the rest of your life, what would that be?";
+        Questions[3] = "If you won a million dollars, what is the first thing you would buy?";
+        Questions[4] = "If you could spend the day with one fictional character, who would it be?";
+        Questions[5] = "If you found a magic lantern and a genie gave you three wishes, what would you wish?";
+
+
+        Random generator = new Random();
+        String x = (String) Array.get(Questions, generator.nextInt(6));
+        random.setText(x);
+
+
+    }
 
 }
 
